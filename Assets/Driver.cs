d2ca5a6552b06a0e3266c00c8f89a28ff011c963
Santiago <scb9908@gmail.com>
2022-09-13 01:46:26 -0500

    feat: game first approach

---
+++ Assets/Driver.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float steerSpeed = 0.1f;
+    [SerializeField] float moveSpeed = 20.0f;
+    [SerializeField] private float slowSpeed = 10.0f;
+    [SerializeField] private float boostSpeed = 20.0f;
+
+    void Start()
+    {
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = slowSpeed;
+    }
+
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }
+    }
+
+}
